2f94f6719eecb470e3ae9c7ec46a5f148ca0c7c1
Samuel Degueldre <sad@odoo.com>
2022-12-06 06:12:57 +0100

    day-6

---
+++ 2022/day-6/puzzle-1.js
@@ -0,0 +1,9 @@
+import { assertEqual, getInputs, extractLines, window } from "../../utils.js";
+const { input, testInput } = getInputs(import.meta.url);
+
+function solution(input) {
+    return window([...input], 4).findIndex((w) => new Set(w).size === 4) + 4;
+}
+
+assertEqual(testInput.split("\n").map(solution), [7, 5, 6, 10, 11]);
+console.log(solution(input)); // 1850
---
+++ 2022/day-6/puzzle-2.js
@@ -0,0 +1,9 @@
+import { assertEqual, getInputs, extractLines, window } from "../../utils.js";
+const { input, testInput } = getInputs(import.meta.url);
+
+function solution(input) {
+    return window([...input], 14).findIndex((w) => new Set(w).size === 14) + 14;
+}
+
+assertEqual(testInput.split("\n").map(solution), [19, 23, 23, 29, 26]);
+console.log(solution(input)); // 2823
