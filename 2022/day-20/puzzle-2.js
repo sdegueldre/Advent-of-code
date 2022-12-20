07ef5f2a4ae8fe41e95cb59ba6f52b37ce0e0246
Samuel Degueldre <sad@odoo.com>
2022-12-20 08:31:49 +0100

    day-20

---
+++ 2022/day-20/puzzle-2.js
@@ -0,0 +1,20 @@
+import { assertEqual, getInputs, sum, zip, product, logThrough, enumerate, mod } from "../../utils.js";
+const { input, testInput } = getInputs(import.meta.url);
+
+function solution(input) {
+    const lines = input.split("\n").map((n, i) => [i, +n * 811589153]);
+    for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < lines.length; i++) {
+            const toMove = lines.findIndex(([origIndex]) => origIndex === i);
+            const item = lines[toMove];
+            const amount = item[1];
+            lines.splice(lines.indexOf(item), 1);
+            lines.splice(mod(toMove + amount, lines.length), 0, [...item]);
+        }
+    }
+    const indexOfZero = lines.findIndex(item => item[1] === 0);
+    return sum([1000, 2000, 3000].map(item => lines[mod(item + indexOfZero, lines.length)][1]));
+}
+
+assertEqual(solution(testInput), 1623178306);
+console.log(solution(input)); // 17113168880158
