3f900cbcf08c13fa8037d81a7b7b45aa2299048b
Samuel Degueldre <sad@odoo.com>
2022-12-25 08:12:21 +0100

    day-25 p1

---
+++ 2022/day-25/puzzle-1.js
@@ -0,0 +1,43 @@
+import { assertEqual, getInputs, sum, zip, product, logThrough, enumerate } from "../../utils.js";
+const { input, testInput } = getInputs(import.meta.url);
+
+function solution(input) {
+    const total = sum(input.split("\n").map(fromSnafu));
+    return toSnafu(total);
+}
+
+const weights = {
+    "=": -2,
+    "-": -1,
+    "0":  0,
+    "1":  1,
+    "2":  2,
+}
+function fromSnafu(num) {
+    return num.split("").reverse().reduce((a, d, i) => a + 5**i * weights[d], 0);
+}
+
+const toDigit = {
+     "-2": "=",
+     "-1": "-",
+     "0": "0",
+     "1": "1",
+     "2": "2",
+}
+function toSnafu(num) {
+    const digits = [2];
+    while (fromSnafu(digits.join("")) < num) digits.push(2);
+    for (let i = 0; i < digits.length; i++) {
+        while(digits[i] > -2) {
+            digits[i]--;
+            if (fromSnafu(digits.map(d => toDigit[d]).join("")) < num) {
+                digits[i]++;
+                break;
+            }
+        }
+    }
+    return digits.map(d => toDigit[d]).join("")
+}
+
+assertEqual(solution(testInput), "2=-1=0");
+console.log(solution(input)); // output
