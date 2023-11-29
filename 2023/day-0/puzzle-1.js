5963d5a9e324223d797c3f6e3e6dae0a10b4d8b3
Samuel Degueldre <sad@odoo.com>
2023-11-29 10:56:52 +0100

    scaffolding for 2023

---
+++ 2023/day-0/puzzle-1.js
@@ -0,0 +1,11 @@
+import { assertEqual, getInputs, sum, zip, product, logThrough, enumerate } from "../../utils.js";
+export const { input, testInput } = getInputs(import.meta.url);
+
+export const testCases = [
+    { input: testInput, expected: "placeholder" },
+];
+
+export function solve(input) {
+    const lines = input.split("\n");
+}
+export const solution = undefined;
