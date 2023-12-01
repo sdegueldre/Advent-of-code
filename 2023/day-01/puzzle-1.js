250342adcd8ac5289df6499ee4f52bce707955bf
Samuel Degueldre <sad@odoo.com>
2023-12-01 07:16:25 +0100

    2023 day 1: clean up part 1 with getNums and .at

--- 2023/day-01/puzzle-1.js
+++ 2023/day-01/puzzle-1.js
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-import { assertEqual, sum, zip, product, logThrough, enumerate } from "../../utils.js";
+import { assertEqual, sum, zip, product, logThrough, enumerate, getNums } from "../../utils.js";
 
 const testInput = readFileSync(new URL("./puzzle-1.test", import.meta.url), "utf-8");
 export const testCases = [[testInput, 142]];
@@ -8,15 +8,8 @@ export function solve(input) {
     return sum(
         input
             .split("\n")
-            .map(
-                (l) =>
-                    +[...l]
-                        .filter((c) => c.match(/\d/))
-                        .map((n) => +n)
-                        .filter((_, i, a) => i === 0 || i === a.length - 1)
-                        .join("")
-            )
-            .map((n) => (n < 10 ? +`${n}${n}` : n))
+            .map(getNums)
+            .map((nums) => 10 * nums.at(0) + nums.at(-1))
     );
 }
 export const solution = 53386;
