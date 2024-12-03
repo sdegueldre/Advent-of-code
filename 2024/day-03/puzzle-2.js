6b4a61ffd924855ee6dd6adc337ea2b69f02fc99
odoo <odoo@sad-laptop.lan>
2024-12-03 13:22:30 -0500

    use dotall modifier instead of removing newlines

--- 2024/day-03/puzzle-2.js
+++ 2024/day-03/puzzle-2.js
@@ -5,8 +5,7 @@ const testInput = readFileSync(new URL("./puzzle-2.test", import.meta.url), "utf
 export const testCases = [[testInput, 48]];
 
 export function solve(input) {
-    input = input.split("\n").join("");
-    input = input.replace(/don't\(\).*?(do\(\)|$)/g,'');
+    input = input.replace(/don't\(\).*?(do\(\)|$)/gs,'');
     return sum(input.match(/mul\(\d+,\d+\)/g).map(getNums).map(([a, b]) => a * b))
 }
 export const solution = 92626942;
